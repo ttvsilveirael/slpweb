3dff033743be029df5d133d1ffbe25fc97c287df
VictorADAB <victorbarros_ita@hotmail.com>
2022-05-31 20:47:38 -0300

    Menu Lateral

---
+++ src/app/components/menu-item/menu_itemcomponent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-item',
+  templateUrl: './menu_item.component.html',
+  styleUrls: ['./menu_item.component.scss']
+})
+export class MenuItemComponent {
+}
