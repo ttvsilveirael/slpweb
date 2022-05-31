d544ac7bae9cbd620c5a4f8351479378e70f6928
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-05-31 19:20:34 -0300

    create home

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
+import { HomeComponent } from "./home/home.component";
 
 
 @NgModule({
     declarations: [
+        HomeComponent,
     ],
     exports: [
+        HomeComponent,
     ],
     imports: [
         BrowserModule,
