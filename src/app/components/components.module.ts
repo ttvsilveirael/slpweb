91b94268449c0e2a86c246224736d5e774b753f9
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-05-30 21:23:07 -0300

    components module

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+
+
+@NgModule({
+    declarations: [
+    ],
+    exports: [
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        CommonModule,
+    ],
+})
+export class ComponentsModule { }
