0fb7faba76a3a41d754eb8c7a8bfbb2ad792ff83
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-06-01 19:26:02 -0300

    update small fixes

--- src/app/components/menu-item/menu_itemcomponent.ts
+++ src/app/components/menu-item/menu_itemcomponent.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'menu-item',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu_item.component.scss']
 })
 export class MenuItemComponent {
+  menuItems = ['Aula', 'Perfil', 'Agenda', 'Sobre'];
+  constructor(private router:Router){}
+  
+  abrirPagina(item:String){
+    this.router.navigate(['/' + item]);
+  }
 }
