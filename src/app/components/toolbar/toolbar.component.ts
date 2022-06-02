12bf8bf4b2d7ae6027afe7d3bf58f83954e49566
Dizeritus <!dizeritus@hotmail.com>
2022-06-02 19:44:36 -0300

    evento click menu

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,23 +1,13 @@
 import { ViewChild } from '@angular/core';
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'toolbar',
   templateUrl: 'toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 
 })
-export class ToolbarComponent{
-  @ViewChild(Trigger)
-  Trigger: ToolbarComponent = new ToolbarComponent;
+export class ToolbarComponent {
 
-  someMethod() {
-    this.Trigger.openMenu();
-  }
-  openMenu() {
-    throw new Error('Method not implemented.');
-  }
-}
-function Trigger(_trigger: any) {
-  throw new Error('Function not implemented.');
+  showmenu = true;
 }
 
